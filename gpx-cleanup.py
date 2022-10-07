398a977faa2216a2ea98c677538a04a73fbfe171
Marc Druckenmueller <marc.druckenmueller@gmail.com>
2022-10-07 13:07:38 +0000

    Add gpx cleanup

---
+++ gpx-cleanup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import gpxpy
+import gpxpy.gpx
+
+
+for dirname in os.listdir("tracks"):
+    full_dirname = os.path.join("tracks", dirname)
+    for filename in os.listdir(full_dirname):
+        print(filename)
+        f = os.path.join(full_dirname, filename)
+        fh = open(f, 'r')
+        gpx = gpxpy.parse(fh)
+        fh.close()
+        gpx.creator = 'gpx.py -- https://github.com/tkrajina/gpxpy'
+        gpx.link = None
+        gpx.time = None
+        gpx.author_name = None
+        gpx.name = filename[0:-4]
+        gpx.extensions = []
+        gpx.metadata_extensions = []
+        for t in gpx.tracks:
+            t.name = None
+            t.type = None
+            t.description = None
+        fh = open(f, 'w')
+        fh.write(gpx.to_xml())
+        fh.close()
+        exit(0)
+
